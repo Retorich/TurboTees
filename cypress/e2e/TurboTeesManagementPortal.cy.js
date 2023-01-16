6196017e4d4ee0e1d81731d1287a2f462c5c9a7c
Retorich <Retorich@gmail.com>
2023-01-16 07:27:44 +0000

    minor changes to extend test database and test

--- cypress/e2e/TurboTeesManagementPortal.cy.js
+++ cypress/e2e/TurboTeesManagementPortal.cy.js
@@ -94,7 +94,7 @@ describe("Visit Product Management Portal - initial test block will fail if: Tes
         cy.get("#removeButton").click();
       });
 
-    cy.get("ul li").should("have.length", 2);
+    cy.get("ul li").should("have.length", 5);
 
     cy.get("#previewButton").click();
 
